8485cd61f8dcaa3905a97d1612a69deed839fd21
rbeiley2 <roenbeiley@gmail.com>
2025-09-08 21:23:06 -0400

    Finished!

--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,22 @@
+score = 0
 
+# receive numbers
+arrests = int(input("Prior arrests: "))
+if arrests >= 5:
+    score += 1
+
+if arrests >= 2:
+    score += 1
+
+ordinance = input("Prior arrests for local ordinance (Y/N): ")
+if ordinance == 'Y':
+    score += 1
+
+release_age = int(input("Age at release: "))
+if release_age >= 18 and release_age <= 24:
+    score += 1
+elif release_age >= 40:
+    score -= 1
+
+# print score
+print ("The recidivism risk score is", score)
